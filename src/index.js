64bcb0cb37ef0ddc42e268c9ea49772feb00edaa
reg <erg@greg.rer>
2024-08-20 04:40:39 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import './index.scss';
+import HotelsSearch from './routes/listings/HotelsSearch';
+import UserProfile from './routes/user-profile/UserProfile';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Home from './routes/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import { makeServer } from './mirage/mirageServer';
+import HotelDetails from './routes/hotel-details/HotelDetails';
+import Login from './routes/login/Login';
+import Register from './routes/register/Register';
+import AboutUs from './routes/about-us/AboutUs';
+import BaseLayout from './routes/layouts/base-layout/BaseLayout';
+import ForgotPassword from './routes/forgot-password/ForgotPassword';
+import Checkout from './routes/checkout/Checkout';
+import BookingConfirmation from './routes/booking-confimation/BookingConifrmation';
 
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+// if (process.env.NODE_ENV === 'development') {
+//   makeServer();
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+makeServer();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <BaseLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/hotels',
+        element: <HotelsSearch />,
+      },
+      {
+        path: '/about-us',
+        element: <AboutUs />,
+      },
+      {
+        path: '/user-profile',
+        element: <UserProfile />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/hotel/:hotelId',
+        element: <HotelDetails />,
+      },
+      {
+        path: '/forgot-password',
+        element: <ForgotPassword />,
+      },
+      {
+        path: '/checkout',
+        element: <Checkout />,
+      },
+      {
+        path: '/booking-confirmation',
+        element: <BookingConfirmation />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
