8702c217be1afa3788cb576e5844b82defa861ba
reg <erg@greg.rer>
2025-01-18 03:26:49 +0700

    1

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App></App>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 
 );
